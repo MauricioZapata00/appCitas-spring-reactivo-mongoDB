4452b144ab56b63f2c723c5e1162ae3be4228f3f
tamagochita <mevere66@gmail.com>
2022-03-29 20:04:54 -0500

    Algunas pruebas implementadas

--- src/test/java/com/springBajo8/springBajo8/service/impl/citasReactivaServiceImplTest.java
+++ src/test/java/com/springBajo8/springBajo8/service/impl/citasReactivaServiceImplTest.java
@@ -1,9 +1,12 @@
 package com.springBajo8.springBajo8.service.impl;
 
 import com.springBajo8.springBajo8.domain.CitasDTOReactiva;
+import org.assertj.core.api.Assert.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -23,17 +26,40 @@ CitasReactivaServiceImpl citasReactivaServiceImpl;
         cita.setEstadoReservaCita("activa");
 
         Mono<CitasDTOReactiva> citaGuardada = citasReactivaServiceImpl.save(cita);
-      //   StepVerifier.create(citaGuardada).expectNext(citasReactivaServiceImpl.cancelarCita(cita.getId(),cita)).verifyComplete();
+       //StepVerifier.create(citaGuardada).expectNext(citasReactivaServiceImpl.cancelarCita(cita.getId(),cita)).verifyComplete();
 
 
+    }
+
+    @Test
+    void buscarPorIdPacienteTest(){
+        CitasDTOReactiva cita = new CitasDTOReactiva();
+        cita.setIdPaciente("1");
+        cita.setHoraReservaCita("10:30 am");
+        cita.setNombrePaciente("Mary");
+        cita.setNombreMedico("Victor");
+        citasReactivaServiceImpl.save(cita);
 
 
+        Flux<CitasDTOReactiva> paciente = citasReactivaServiceImpl.findByIdPaciente("1");
+        StepVerifier.create(paciente).expectNext(cita).verifyComplete();
+    }
 
+    @Test
+    void deleteCitaByIdTest (String id){
+        CitasDTOReactiva cita = new CitasDTOReactiva();
+        cita.setIdPaciente("1");
+        cita.setHoraReservaCita("10:30 am");
+        cita.setNombrePaciente("Mary");
+        cita.setNombreMedico("Victor");
+        citasReactivaServiceImpl.save(cita);
+        citasReactivaServiceImpl.delete(cita.getId());
 
+       // Mono<CitasDTOReactiva> cita1=  citasReactivaServiceImpl.save(cita);
 
-
-
-
+        Mono<CitasDTOReactiva> citaPaBorrar = citasReactivaServiceImpl.delete(cita.getId());
+        //StepVerifier.create(citaPaBorrar).expectNext(citasReactivaServiceImpl.delete("1")).verifyComplete();
+        StepVerifier.create(citaPaBorrar).verifyComplete();
 
     }
 }
